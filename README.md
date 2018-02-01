225147158571baecf2c5476829ebe5c33beadccc
manjeet <mann3774@gmail.com>
2018-02-01 11:33:10 +0530

    all files added

---
+++ README.md
@@ -0,0 +1,8 @@
+A FreeCodeCamp API project.
+
+
+Returns user's information like ip address, language used and OS type
+
+
+how to use:
+
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');                  // accepts is present in express
+const useragent = require('express-useragent');
+const app = express();                               // instantiate the express
+
+app.use(useragent.express());                        // instantiating the dependency in express
+
+app.get('/api/whoami', function(req, res){
+	
+	
+	var ip = req.ip;
+	var lang = req.acceptsLanguages();
+	var software = req.useragent;                    // get user information using useragent dependency
+    software = software.platform+", "+software.os;
+    res.json({'ipaddress' : ip, 'language': lang[0], 'software': software}); // can use /res.send({ipaddress : ip});/ also    
+	
+});
+
+
+app.listen(process.env.PORT || 3000);
